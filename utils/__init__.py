462f7d0de55dcc35abf816b0638b7971c2cc2682
luyandamncube <lmn3333@gmail.com>
2025-08-14 17:56:51 +0200

    Added basic ETF indicators

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
 # QuantTrade/utils/__init__.py
 from .duck import to_bt_daily_duckdb, to_bt_minute_duckdb, build_1m
-from .features import add_mas_duckdb, add_rsi_duckdb
+from .features import add_mas_duckdb, add_rsi_duckdb, add_emas_duckdb
 from .charts import render_lightweight_chart, render_rsi_lightweight  # if present
 
 __all__ = [
@@ -8,6 +8,7 @@ __all__ = [
     "to_bt_minute_duckdb",
     "build_1m",
     "add_mas_duckdb",
+    "add_emas_duckdb",
     "add_rsi_duckdb",
     "render_lightweight_chart",
     "render_rsi_lightweight",
